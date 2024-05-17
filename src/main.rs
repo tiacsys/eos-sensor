664cc8673d64b58d40090c50b6f787ff3cc27792
Sarah Renkhoff <sarah.renkhoff@navimatix.de>
2024-05-17 08:58:19 +0200

    Get sensor data and send single data points to server

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod ws;
 
 use esp_backtrace as _;
 
-use esp_hal::gpio::{GpioPin, Output, PushPull};
+use esp_hal::gpio::{AnyPin, Output, PushPull, InputOutputAnalogPinType};
 use esp_hal::prelude::*;
 use esp_hal::rng::Rng;
 use esp_hal::{
@@ -17,16 +17,25 @@ use esp_hal::{
         executor::Executor,
     },
     peripherals::Peripherals,
+    peripherals::I2C0,
     timer::TimerGroup,
+    i2c::I2C,
+    Blocking,
 };
 use esp_wifi::wifi::{self, WifiController, WifiDevice, WifiEvent, WifiStaDevice, WifiState, WifiError};
 
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
+use embassy_time::{Duration, Instant, Timer};
 use embassy_net::{
     Stack,
     StackResources,
 };
+
+use lsm9ds1::{LSM9DS1Init, LSM9DS1};
+use lsm9ds1::interface::I2cInterface;
+
+use hecate_protobuf as proto;
+use hecate_protobuf::Message;
 
 use static_cell::make_static;
 
@@ -46,8 +55,11 @@ struct Config {
     device_id: &'static str,
 }
 
+#[macro_use]
 extern crate alloc;
 use core::mem::MaybeUninit;
+
+type SensorType = LSM9DS1<I2cInterface<I2C<'static, I2C0, Blocking>>>;
 
 #[global_allocator]
 static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
@@ -81,7 +93,16 @@ fn main() -> ! {
 
     // Initialize LED
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
-    let led = io.pins.gpio13.into_push_pull_output();
+    let led = io.pins.gpio13.into_push_pull_output().degrade();
+
+    // Sensor setup
+    let mut qwiic_power = io.pins.gpio2.into_push_pull_output();
+    qwiic_power.set_high();
+    let ag_addr = lsm9ds1::interface::i2c::AgAddress::_2;
+    let mag_addr = lsm9ds1::interface::i2c::MagAddress::_2;
+    let sensor_i2c = I2C::new(peripherals.I2C0, io.pins.gpio22, io.pins.gpio20, 100.kHz(), &clocks, None);
+    let sensor_interface = lsm9ds1::interface::I2cInterface::init(sensor_i2c, ag_addr, mag_addr);
+    let sensor = LSM9DS1Init::default().with_interface(sensor_interface);
 
     // Initialize WiFi
     let timer = esp_hal::timer::TimerGroup::new(peripherals.TIMG1, &clocks, None).timer0;
@@ -101,7 +122,7 @@ fn main() -> ! {
     let executor = make_static!(Executor::new());
 
     executor.run(|spawner| {
-    _ = spawner.spawn(networking_task(wifi_interface, wifi_controller, rng, led));
+    _ = spawner.spawn(networking_task(wifi_interface, wifi_controller, rng, led, sensor));
     });
 }
 
@@ -111,7 +132,7 @@ async fn net_stack_task(stack: &'static Stack<WifiDevice<'static, WifiStaDevice>
 }
 
 #[embassy_executor::task]
-async fn connection_task(mut controller: WifiController<'static>, mut led: GpioPin<Output<PushPull>, 13>) -> () {
+async fn connection_task(mut controller: WifiController<'static>, mut led: AnyPin<Output<PushPull>, InputOutputAnalogPinType>) -> () {
 
     loop {
 
@@ -159,7 +180,13 @@ async fn connection_task(mut controller: WifiController<'static>, mut led: GpioP
 }
 
 #[embassy_executor::task]
-async fn networking_task(interface: WifiDevice<'static, WifiStaDevice>, controller: WifiController<'static>, rng: Rng, led: GpioPin<Output<PushPull>, 13>) {
+async fn networking_task(
+    interface: WifiDevice<'static, WifiStaDevice>,
+    controller: WifiController<'static>,
+    rng: Rng,
+    led: AnyPin<Output<PushPull>, InputOutputAnalogPinType>,
+    mut sensor: SensorType,
+) {
 
     // Create network stack
     let config = embassy_net::Config::dhcpv4(Default::default());
@@ -211,4 +238,33 @@ async fn networking_task(interface: WifiDevice<'static, WifiStaDevice>, controll
         log::info!("Sent id");
     }
     
+    sensor.begin_accel().expect("Failed to initialize accelerometer");
+    sensor.begin_gyro().expect("Failed to initialize gyroscope");
+    sensor.begin_mag().expect("Failed to initialize magnetometer");
+
+    let start = Instant::now();
+
+    loop {
+        let acc = sensor.read_accel();
+        let gyro = sensor.read_gyro();
+        let mag = sensor.read_mag();
+
+        if let (Ok((ax, ay, az)), Ok((gx, gy, gz)), Ok((mx, my, mz))) = (acc, gyro, mag) {
+            let time = Instant::now() - start;
+            let sample = proto::SensorDataSample {
+                time: time.as_millis() as f32 / 1000.0,
+                acceleration: proto::Acceleration{ x: ax, y: ay, z: az },
+                magnetometer: proto::MagnetometerData { x: mx, y: my, z: mz },
+                gyroscope: proto::GyroscopeData { x: gx, y: gy, z: gz },
+            };
+
+            let samples = proto::SensorData {
+                samples: vec![sample],
+            }.encode_to_vec();
+
+            _ = websocket.send_binary(&samples).await;   
+        }
+    }
+
+
 }

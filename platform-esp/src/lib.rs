d58a165e66654089c8f9dffc78190b22f7d4d915
Sarah Renkhoff <sarah.renkhoff@navimatix.de>
2024-05-22 13:53:18 +0200

    Add type safety checks to platform-esp
    These can't be run with `cargo test` since test requires `std`, while
    `esp-hal` cannot be built for linux targets. Instead, run
    
    `cargo check --features type-checks`
    
    to verify the types meet the (non-enforced) trait bounds.

--- platform-esp/src/lib.rs
+++ platform-esp/src/lib.rs
@@ -11,3 +11,21 @@ use lsm9ds1::{LSM9DS1, interface::I2cInterface};
 pub type Sensor = LSM9DS1<I2cInterface<I2C<'static, I2C0, Blocking>>>;
 pub type NetworkDevice = WifiDevice<'static, WifiStaDevice>;
 pub use esp_hal::rng::Rng;
+
+#[cfg(feature="type-checks")]
+mod type_checks {
+    #![allow(dead_code)]
+    use rand_core::RngCore;
+    use embassy_net_driver::Driver;
+    
+    fn implements_rngcore<T: RngCore>() {}
+    fn implements_driver<T: Driver>() {}
+    
+    fn rng_implements_rngcore() {
+        implements_rngcore::<super::Rng>();
+    }
+
+    fn network_device_implements_driver() {
+        implements_driver::<super::NetworkDevice>();
+    }
+}

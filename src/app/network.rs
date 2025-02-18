11389cc06833cf6515de765a96c6dcdb1deeafa8
Sarah Renkhoff <sarah.renkhoff@navimatix.de>
2025-02-18 09:57:07 +0100

    Restructure data sending

--- src/app/network.rs
+++ src/app/network.rs
@@ -1,6 +1,7 @@
 use super::{AppConfig, NetworkInterface, Rng};
 use crate::make_static;
 use crate::ws;
+use crate::ws::WebSocketBuffers;
 use hecate_protobuf as proto;
 
 use proto::Message;
@@ -8,9 +9,8 @@ use proto::Message;
 use alloc::vec::Vec;
 use anyhow::{anyhow, Error, Result};
 use embassy_executor::Spawner;
-use embassy_net::StackResources;
+use embassy_net::{Stack, StackResources};
 use embassy_time::Timer;
-use futures::future::TryFutureExt;
 use rand_core::RngCore;
 use ringbuffer::RingBuffer;
 
@@ -29,8 +29,34 @@ pub async fn network_task(
     let (stack, runner) = embassy_net::new(interface, config, stack_resources, seed);
 
     let spawner = Spawner::for_current_executor().await;
-    _ = spawner.spawn(net_stack_task(runner));
+    spawner
+        .spawn(net_stack_task(runner))
+        .expect("Failed to spawn network stack task");
 
+    let mut ws_buffers = WebSocketBuffers::<4096>::new();
+
+    loop {
+        wait_for_connection(stack).await;
+        if let Ok(mut websocket) =
+            try_open_websocket(&mut ws_buffers, stack, &app_config, rng).await
+        {
+            log::info!("Websocket open!");
+
+            if let Err(e) = websocket.send_text(app_config.device_id).await {
+                log::error!("Failed to send device ID: {:?}", e);
+                continue;
+            }
+
+            // This keeps looping until an error occurs.
+            if let Err(e) = send_data_from_ringbuffer(ringbuffer.clone(), &mut websocket).await {
+                log::error!("{:?}", e);
+                continue;
+            }
+        }
+    }
+}
+
+async fn wait_for_connection(stack: Stack<'_>) {
     log::info!("Waiting for network link");
     loop {
         if stack.is_link_up() {
@@ -48,50 +74,55 @@ pub async fn network_task(
         }
         Timer::after_secs(1).await;
     }
+}
 
-    let mut ws_buffers = ws::WebSocketBuffers::<2048>::new();
-    let mut websocket = ws::WebSocket::new(
+/// Tries to open a websocket connection according to the device configuration.
+async fn try_open_websocket<'a, const S: usize>(
+    ws_buffers: &'a mut ws::WebSocketBuffers<S>,
+    stack: Stack<'a>,
+    app_config: &AppConfig,
+    rng: Rng,
+) -> Result<ws::WebSocket<'a, Rng, S>, Error> {
+    ws::WebSocket::new(
         app_config.ws_host,
         app_config.ws_port,
         app_config.ws_endpoint,
         stack,
         rng,
-        &mut ws_buffers,
+        ws_buffers,
     )
     .await
-    .expect("Failed to connect");
+    .map_err(|e| anyhow!("Websocket error: {:?}", e))
+}
 
-    log::info!("Websocket open");
-
-    if let Ok(()) = websocket.send_text(app_config.device_id).await {
-        log::info!("Sent id");
-    }
-
+#[doc(hidden)]
+/// Tries to get data from a ringbuffer and sends it via provided websocket. Keeps grabbing data
+/// until an error occurs.
+async fn send_data_from_ringbuffer<const S: usize>(
+    ringbuffer: super::RingBufferMutex,
+    websocket: &mut ws::WebSocket<'_, Rng, S>,
+) -> Result<(), Error> {
     loop {
+
         let samples = {
             let mut ringbuffer = ringbuffer.lock().await;
-            ringbuffer.drain().take(50).collect::<Vec<_>>()
+            ringbuffer.drain().take(32).collect::<Vec<_>>()
         };
 
         let data = proto::SensorData { samples };
 
-        // By the Twelve I wish I could just >>= here instead of juggling that async move block
-        _ = encode_data(data)
-            .and_then({
-                // Need to borrow `websocket` outside of the closure so the move doesn't consume it:
-                let websocket = &mut websocket;
-                |encoded_data| async move { send_data(&encoded_data, websocket).await }
-            })
-            .await
-            .inspect_err(|e| log::warn!("{:?}", e));
+        match encode_data(data) {
+            Ok(encoded_data) => send_data_over_websocket(&encoded_data, websocket).await?,
+            Err(e) => log::error!("{:?}", e),
+        };
 
         Timer::after_millis(100).await;
     }
 }
 
 #[doc(hidden)]
-/// Takes some data and tries to encode it. Async to allow chaining via TryFutureExt's .and_then()
-async fn encode_data<T>(data: T) -> Result<Vec<u8>, Error>
+/// Takes some data and tries to encode it. Maps any error that might occur to the anyhow one.
+fn encode_data<T>(data: T) -> Result<Vec<u8>, Error>
 where
     T: Message,
 {
@@ -104,9 +135,10 @@ where
 }
 
 #[doc(hidden)]
-/// Takes a buffer of bytes and tries to send them via a websocket connection.
+/// Takes a buffer of bytes and tries to send them via a websocket connection. Maps any error to
+/// the anyhow one.
 use crate::ws::WebSocket;
-async fn send_data<T, const N: usize>(
+async fn send_data_over_websocket<T, const N: usize>(
     data: &[u8],
     websocket: &mut WebSocket<'_, T, N>,
 ) -> Result<(), Error>
@@ -115,8 +147,8 @@ where
 {
     websocket
         .send_binary(data)
-        .map_err(|e| anyhow!("Websocket error: {:?}", e))
         .await
+        .map_err(|e| anyhow!("Websocket error: {:?}", e))
 }
 
 /// Task that the network stack is running on.
